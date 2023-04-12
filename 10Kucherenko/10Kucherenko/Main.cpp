7a300b0e14bbc20a07ab83d54208656f42a66d66
dan_kucherenko <daniil.ukraine@gmail.com>
2023-04-12 14:49:12 +0300

    Refactored

--- 10Kucherenko/10Kucherenko/Main.cpp
+++ 10Kucherenko/10Kucherenko/Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "QueueArray.h"
+#include "QueueList.h"
 #include "QueueSizedArray.h"
 
 using namespace std;
@@ -9,8 +10,10 @@ int main(void)
 {
 	QueueSizedArray<int> q(3);
 	QueueArray<char> q2;
+	QueueList<double> q3;
+
 	{
-		cout << "Sized queue based on array testing: " << endl;
+		cout << endl << "Sized queue based on array testing: " << endl;
 		cout << "Queue size: " << q.size() << ". Is it empty? " << boolalpha << q.empty() << endl;
 		cout << "Queue capacity: " << q.capacity() << endl;
 		try
@@ -24,6 +27,7 @@ int main(void)
 			cout << "Is the queue full with capacity " << q.capacity() << "? " << boolalpha << q.full() << endl;
 			q.print();
 			q.pop();
+			cout << "Element popped" << endl;
 			q.print();
 			cout << "Queue capacity: " << q.capacity() << endl;
 			cout << "Queue size: " << q.size() << endl;
@@ -31,6 +35,31 @@ int main(void)
 		catch (const QueueSizedArray<int>::BadQueueSizedArray& bqsa)
 		{
 			bqsa.exceptionMessage();
+		}
+		cout << "---------------------------------------------" << endl;
+	}
+	{
+		cout << endl << "Queue based on list testing: " << endl;
+		cout << "Queue size: " << q3.size() << ". Is it empty? " << boolalpha << q3.empty() << endl;
+		cout << "Queue capacity: " << q3.capacity() << endl;
+		try
+		{
+            q3.put(3.4);
+            q3.put(3.5);
+            q3.put(3.6);
+            q3.put(3.7);
+            q3.put(3.8);
+            cout << "Queue front: " << q3.front() << endl;
+            q3.print();
+            cout << "Queue size: " << q3.size() << endl;
+            q3.pop();
+			cout << "Element popped" << endl;
+            q3.print();
+
+		}
+		catch (const QueueList<double>::BadQueueList& bql)
+		{
+			bql.exceptionMessage();
 		}
 		cout << "---------------------------------------------" << endl;
 	}
@@ -53,6 +82,7 @@ int main(void)
 			cout << "Is the queue full with capacity " << q2.capacity() << "? " << boolalpha << q2.full() << endl;
 			q2.print();
 			q2.pop();
+			cout << "Element popped" << endl;
 			q2.print();
 			cout << "Queue capacity: " << q2.capacity() << endl;
 			cout << "Queue size: " << q2.size() << endl;
@@ -68,7 +98,6 @@ int main(void)
 		}
 		cout << "---------------------------------------------" << endl;
 	}
-
 
 	return 0;
 }
--- 10Kucherenko/10Kucherenko/QueueSizedArray.h
+++ 10Kucherenko/10Kucherenko/QueueSizedArray.h
@@ -1,114 +1,119 @@
 #pragma once
+
 #include "Array.h"
 #include "IQueue.h"
 #include "QueueArray.h"
 
-template <class T>
-class QueueSizedArray : public IQueue<T>
-{
+template<class T>
+class QueueSizedArray : public IQueue<T> {
 private:
-	size_t _size;
-	Array<T>* _arr;
+    size_t _size;
+    Array<T> *_arr;
 
-	// unused methods
-	QueueSizedArray(const QueueSizedArray&);
-	QueueSizedArray& operator=(const QueueSizedArray&);
+    // unused methods
+    QueueSizedArray(const QueueSizedArray &);
+
+    QueueSizedArray &operator=(const QueueSizedArray &);
 
 public:
-	class BadQueueSizedArray;
-	explicit QueueSizedArray(const size_t capacity);
-	~QueueSizedArray() override;
-	inline bool empty() const override { return size() == 0; }
-	inline bool full() const override { return size() == capacity(); }
-	const T& front() const override;
-	void pop() override;
-	void put(const T& value) override;
-	inline size_t capacity() const override { return _arr->size(); }
-	inline size_t size() const override { return _size; }
+    class BadQueueSizedArray;
 
-	inline void print() const
-	{
-		std::cout << "Printing queue:" << std::endl; // друк повідомлення перед виводом черги
-		for (size_t i = 0; i < size(); i++)
-		{
-			std::cout << (*_arr)[i] << " ";
-		}
-		std::cout << std::endl;
-	}
+    explicit QueueSizedArray(const size_t capacity);
+
+    ~QueueSizedArray();
+
+    inline bool empty() const { return size() == 0; }
+
+    inline bool full() const { return size() == capacity(); }
+
+    const T &front() const;
+
+    void pop();
+
+    void put(const T &value);
+
+    inline size_t capacity() const { return _arr->size(); }
+
+    inline size_t size() const { return _size; }
+
+    inline void print() const; // method to output the queue for testing
 };
 
-template <class T>
-std::ostream& operator<<(std::ostream& os, const QueueSizedArray<T>& q);
+template<class T>
+void QueueSizedArray<T>::print() const {
+    for (size_t i = 0; i < size(); i++)
+        std::cout << (*_arr)[i] << ' ';
+    std::cout << std::endl;
+}
+
+template<class T>
+std::ostream &operator<<(std::ostream &os, const QueueSizedArray<T> &q);
 
 #pragma region Constructor/Destructor
-template <class T>
-QueueSizedArray<T>::QueueSizedArray(const size_t capacity) : _size(0), _arr(new Array<T>(capacity))
-{
+
+template<class T>
+QueueSizedArray<T>::QueueSizedArray(const size_t capacity) : _size(0), _arr(new Array<T>(capacity)) {
 }
 
-template <class T>
-QueueSizedArray<T>::~QueueSizedArray()
-{
-	delete _arr;
+template<class T>
+QueueSizedArray<T>::~QueueSizedArray() {
+    delete _arr;
 }
+
 #pragma endregion
 
 #pragma region Class methods
-template <class T>
-const T& QueueSizedArray<T>::front() const
-{
-	if (empty())
-		throw BadQueueSizedArray("Queue is empty, there is no front");
-	return (*_arr)[0];
+
+template<class T>
+const T &QueueSizedArray<T>::front() const {
+    if (empty())
+        throw BadQueueSizedArray("Queue is empty, there is no front");
+    return (*_arr)[0];
 }
 
-template <class T>
-void QueueSizedArray<T>::pop()
-{
-	if (empty())
-		throw BadQueueSizedArray("Queue is empty, there is nothing to pop");
-	--_size;
-	for (size_t i = 0; i < _size; ++i)
-		(*_arr)[i] = (*_arr)[i+1];
+template<class T>
+void QueueSizedArray<T>::pop() {
+    if (empty())
+        throw BadQueueSizedArray("Queue is empty, there is nothing to pop");
+    --_size;
+    for (size_t i = 0; i < _size; ++i)
+        (*_arr)[i] = (*_arr)[i + 1];
 }
 
-template <class T>
-void QueueSizedArray<T>::put(const T& value)
-{
-	if (full())
-		throw BadQueueSizedArray("Queue is already full");
-	(*_arr)[_size++] = value;
+template<class T>
+void QueueSizedArray<T>::put(const T &value) {
+    if (full())
+        throw BadQueueSizedArray("Queue is already full");
+    (*_arr)[_size++] = value;
 }
 
-template <class T>
-std::ostream& operator<<(std::ostream& os, const QueueSizedArray<T>& q)
-{
-	os << '[';
-	if (!q.empty())
-		os << q._arr;
-	return os << ']';
+template<class T>
+std::ostream &operator<<(std::ostream &os, const QueueSizedArray<T> &q) {
+    os << '[';
+    if (!q.empty())
+        os << q._arr;
+    return os << ']';
 }
+
 #pragma endregion
 
 #pragma region BadQueueArray Implementation
-template <class T>
-class QueueSizedArray<T>::BadQueueSizedArray
-{
+
+template<class T>
+class QueueSizedArray<T>::BadQueueSizedArray {
 private:
-	std::string _problem;
+    std::string _problem;
 
 public:
-	explicit BadQueueSizedArray(const std::string problem = "") : _problem(problem)
-	{
-	}
+    explicit BadQueueSizedArray(const std::string problem = "") : _problem(problem) {
+    }
 
-	~BadQueueSizedArray()
-	{
-	}
+    ~BadQueueSizedArray() {
+    }
 
-	void exceptionMessage() const
-	{
-		std::cerr << _problem << std::endl;
-	}
+    void exceptionMessage() const {
+        std::cerr << _problem << std::endl;
+    }
 };
+
 #pragma endregion
